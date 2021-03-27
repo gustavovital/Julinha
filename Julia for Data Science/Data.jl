5619dbce0feb77c3fca076718984d88b856473af
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-03-27 18:00:25 +0000

    finish introduction

--- Julia for Data Science/Data.jl
+++ Julia for Data Science/Data.jl
@@ -14,3 +14,44 @@ using XLSX
 #  About Download
 P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv",
     "programminglanguages.csv")
+
+#=
+readdlm(source,
+    delim::AbstractChar,
+    T::Type,
+    eol::AbstractChar;
+    header=false,
+    skipstart=0,
+    skipblanks=true,
+    use_mmap,
+    quotes=true,
+    dims,
+    comments=false,
+    comment_char='#')
+=#
+
+# For now, lets read as a csv
+P, H = readdlm("programminglanguages.csv", ','; header=true)
+C = CSV.read("programminglanguages.csv", DataFrame)
+
+@show typeof(C)
+@show C[1:10,:]
+@show C[!,:language]
+@show C.year
+
+@show names(C)
+@show describe(C)
+
+@btime P, H = readdlm("programminglanguages.csv", ','; header=true)
+@btime C = CSV.read("programminglanguages.csv", DataFrame)
+
+#  Reading from xlsx file:
+@btime T = XLSX.readdata("Julia for Data Science/Data/zillow_data_download_april2020.xlsx", #file name
+    "Sale_counts_city", #sheet name
+    "A1:F9" #cell range
+    )
+
+# @btime G = XLSX.readtable("Julia for Data Science/Data/zillow_data_download_april2020.xlsx",
+#     "Sale_counts_city")
+G = XLSX.readtable("Julia for Data Science/Data/zillow_data_download_april2020.xlsx",
+    "Sale_counts_city")
