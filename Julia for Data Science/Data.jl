ad76147e4ee61ef9d013a7ee237d3634d52068a1
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-03-27 04:53:37 +0000

    start data science

---
+++ Julia for Data Science/Data.jl
@@ -0,0 +1,16 @@
+#=
+No author? No date? Jst boring ;(
+More stuff
+=#
+
+# Lot of modules i should use, time of class: 40 f*****g 7 minutes
+
+using BenchmarkTools
+using DataFrames
+using DelimitedFiles
+using CSV
+using XLSX
+
+#  About Download
+P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv",
+    "programminglanguages.csv")
