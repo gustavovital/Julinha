94120259168887c6b91198515f843000fda1f085
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-04-08 22:44:03 +0100

    finish LA

--- Julia for Data Science/Linear Algebra.jl
+++ Julia for Data Science/Linear Algebra.jl
@@ -27,3 +27,52 @@ x = A\b
 @show typeof(Atranspose)
 
 # ?Adjoint just work on shit jupyter
+
+# Factorization
+
+# LU factorization
+luA = lu(A)
+@show norm(luA.L*luA.U - luA.P*A)
+
+# Cholensky factorization  - simetrica e definido positivo
+isposdef(A)
+
+cholA = cholesky(A)
+@show cholA.U
+
+factorize(A) == cholA.U # false? precisiono?
+
+# more stuff ..
+diagm(diag(A)) # cria uma matriz diagonal com os valores array
+@show I(3)
+
+S = sprand(5, 5, 2/5)
+S.rowval
+Matrix(S)
+
+S.colptr
+
+# Working w images
+X1 = load("Julia for Data Science\\Data\\khiam-small.jpg")
+@show typeof(X1)
+X1[1, 1]
+X1
+
+Xgray = Gray.(X1)
+
+# mudando tudo.. extraindo RGB da imagem
+R = map(i -> X1[i].r, 1:length(X1))
+R = Float64.(reshape(R, size(X1)...))
+
+G = map(i -> X1[i].g, 1:length(X1))
+G = Float64.(reshape(G, size(X1)...))
+
+B = map(i -> X1[i].b, 1:length(X1))
+B = Float64.(reshape(B, size(X1)...))
+
+z = zeros(size(R)...)
+
+RGB.(R, G, B)
+
+# Esse foi o basico de imagens, nao continuo pois tenho zero interesse (por
+# enqunato) em manipuilação de imagens. 
