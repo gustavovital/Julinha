d2d646b2b7a27f1be2899930786150036bcd802d
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-05-18 14:39:58 +0100

    add first example

--- Convex/portfolio.jl
+++ Convex/portfolio.jl
@@ -4,4 +4,20 @@
 # Pkg.add("Convex")
 # Pkg.add("SCS")
 
-using Convex
+using Convex, SCS
+
+## First example
+# Starting the example - generating random numbers and data
+m = 4
+n = 5
+
+A = randn(m, n)
+b = randn(m, 1)
+x = Variable(n)
+
+## Minimization
+problem = minimize(sumsquares(A * x - b), [x >= 0])
+solve!(problem, SCS.Optimizer)
+
+print(problem.status)
+print(problem.optval)
